16eb123ad9ac3a34f5a6f6bdd2dc7f765a6fefa5
danielgkneto <GBTC440016UR@MC.MONTGOMERYCOLLEGE.EDU>
2019-09-04 11:46:00 -0400

    First commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+//Write an application that assigns 2 to a variable named mynumber and 1.7938 to a variable named myothernumber, and then prints out the values to the console.
+
+public class Main {
+
+    public static void main(String[] args){
+        int mynumber = 2;
+        double myothernumber = 1.7938;
+        System.out.println("My two numbers are " + mynumber + " and " + myothernumber);
+    }
+}
